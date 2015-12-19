32ab001749d51c0cefb5fe4a9b2c9e9100833ad4
ozantrkcn <ozantrkcn@gmail.com>
2015-12-19 20:07:26 +0200

    Database Renewed/Tutorial Paged

---
+++ database/seeds/TutorialTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TutorialTableSeeder extends Seeder{
+
+
+    public function run(){
+
+        $faker = Faker\Factory::create();
+
+
+        foreach(range(1,30) as $index){
+
+
+            DB::table('tutorial')->insert([
+                'title'=> $faker->sentence(30,true),
+                'publisher_id' => $faker->numberBetween(10,10000),
+                'date' => $faker->date('d-m-Y','now'),
+                'tag' => $faker->words(8,true),
+                'rate' => $faker->numberBetween(0,10)
+
+
+            ]);
+        }
+
+    }
+}
