9379b4219285832ed5dc184967f54a4c46a398e4
ozantrkcn <ozantrkcn@gmail.com>
2016-02-19 15:01:12 +0200

    Article-Event-Login Controllers Created Indexs created

---
+++ resources/views/Article/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yasar Ozan
+ * Date: 19-Feb-16
+ * Time: 2:46 PM
+ */
---
+++ resources/views/Article/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yasar Ozan
+ * Date: 19-Feb-16
+ * Time: 2:45 PM
+ */
