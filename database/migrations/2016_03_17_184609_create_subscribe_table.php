c923c4ca7c4f3cd89453060cfabfb3297a353cfd
ozantrkcn <ozantrkcn@gmail.com>
2016-03-19 18:27:50 +0200

    subscriptions

---
+++ database/migrations/2016_03_17_184609_create_subscribe_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscribeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribe', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id');
+            $table->integer('subscriber_id')->unsigned()->index();
+            $table->integer('subscribed_id')->unsigned()->index();
+            $table->rememberToken();
+            $table->timestamps();
+
+            //constraints for table
+            $table->foreign('subscriber_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('subscribed_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscribe', function (Blueprint $table) {
+            $table->dropForeign('subscribes_subscriber_id_foreign');
+            $table->dropForeign('subscribes_subscribed_id_foreign');
+        });
+
+        Schema::drop('subscribe');
+    }
+}
