230b7c898948eca0070127a948de1ca8f4f169b4
Omer Cem Kaya <omercemkaya@gmail.com>
2016-03-31 22:57:41 +0300

    Anasayfa Tasarımı

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,46 @@
 <?php
-$url_css_js="http://localhost/laravel/resources/assets/";
+$url_css_js = "http://localhost/bitirme/WPTE_v1/resources/assets/";
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-
-
 <head>
     <meta charset="utf-8">
     <!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Sedna - A Free HTML5/CSS3 website by Peter Finlan</title>
-    <meta name="description" content="">
+    <title>Sedna: Free HTML5/CSS3 Template by Peter Finlan</title>
+    <meta name="description" content="A free HTML5/CSS3 template made exclusively for Codrops by Peter Finlan"/>
+    <meta name="keywords" content="html5 template, free, css3, one page, animations, agency, portfolio, web design"/>
+    <meta name="author" content="Peter Finlan"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
-    <link rel="stylesheet" href="{{$url_css_js}}css/normalize.min.css">
     <link rel="stylesheet" href="{{$url_css_js}}css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{$url_css_js}}css/normalize.min.css">
     <link rel="stylesheet" href="{{$url_css_js}}css/jquery.fancybox.css">
     <link rel="stylesheet" href="{{$url_css_js}}css/flexslider.css">
     <link rel="stylesheet" href="{{$url_css_js}}css/styles.css">
     <link rel="stylesheet" href="{{$url_css_js}}css/queries.css">
     <link rel="stylesheet" href="{{$url_css_js}}css/etline-font.css">
     <link rel="stylesheet" href="{{$url_css_js}}bower_components/animate.css/animate.min.css">
-    <link rel="stylesheet" href="{{$url_css_js}}css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <script src="{{$url_css_js}}js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-7243260-2']);
+        _gaq.push(['_trackPageview']);
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
 </head>
 <body id="top">
 <!--[if lt IE 8]>
@@ -37,21 +50,20 @@ $url_css_js="http://localhost/laravel/resources/assets/";
     <section class="navigation">
         <header>
             <div class="header-content">
-                <div class="logo"><p>Site Logo Will Come Here</p></div>
+                <div class="logo"><a href="#"><img src="{{$url_css_js}}img/youtestify_small.png" alt="YouTestify Logo"></a>
+                </div>
                 <div class="header-nav">
                     <nav>
                         <ul class="primary-nav">
-
-                            <li><a href="#features">SORU & CEVAP</a></li>
+                            <li><a href="#question">SORU & CEVAP</a></li>
                             <li><a href="#assets">EĞİTİM</a></li>
                             <li><a href="#blog">ETKİNLİKLER</a></li>
                             <li><a href="#download">MAKALELER</a></li>
                         </ul>
                         <ul class="member-actions">
-                            <li id="users"><a href="#login" class="login">GİRİŞ</a></li>
-                            <li id="logout"><a href="/auth/register" class="btn-white btn-small">KAYIT OL</a></li>
+                            <li><a href="#download" class="login">GİRİŞ</a></li>
+                            <li><a href="#download" class="btn-white btn-small">KAYIT OL</a></li>
                         </ul>
-
                     </nav>
                 </div>
                 <div class="navicon">
@@ -64,56 +76,52 @@ $url_css_js="http://localhost/laravel/resources/assets/";
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="hero-content text-center">
-                    <h1>Design, collaborate, win!</h1>
-                    <p class="intro">Introducing “Sedna”. A responsive one page website, designed & developed by Peter Finlan, exclusively for Codrops.</p>
-                    <a href="#" class="btn btn-fill btn-large btn-margin-right">Download</a> <a href="#" class="btn btn-accent btn-large">Learn more</a>
+                    <h1>YouTestify</h1>
+                    <p class="intro">A unique web platform for test engineers.</p>
+                    {{--<a href="#" class="btn btn-fill btn-large btn-margin-right">Download</a>--}}
+                    {{--<a href="#" class="btn btn-accent btn-large">Learn more</a>--}}
                 </div>
             </div>
         </div>
     </div>
-    <div class="down-arrow floating-arrow"><a href="#"><i class="fa fa-angle-down"></i></a></div>
-</section>
-<section class="intro section-padding">
     <div class="container">
         <div class="row">
             <div class="col-md-4 intro-feature">
-                <div class="intro-icon">
-                    <span data-icon="&#xe033;" class="icon"></span>
-                </div>
+                {{--<div class="intro-icon">--}}
+                {{--<span data-icon="&#xe033;" class="icon"></span>--}}
+                {{--</div>--}}
                 <div class="intro-content">
-                    <h5>Easily Customised</h5>
-                    <p>Easily customise Sedna to suit your start up, portfolio or product. Take advantage of the layered Sketch file and bring your product to life.</p>
+                    <h5>Articles & Tutorials</h5>
+                    <p>Browse various informative articles and tutorials to improve your know-how!</p>
+                    <p>Subscribe to your favorite authors to follow articles and blogs that interests you!</p>
                 </div>
             </div>
             <div class="col-md-4 intro-feature">
-                <div class="intro-icon">
-                    <span data-icon="&#xe030;" class="icon"></span>
-                </div>
                 <div class="intro-content">
-                    <h5>Modern Design</h5>
-                    <p>Designed with modern trends and techniques in mind, Sedna will help your product stand out in an already saturated market.</p>
+                    <h5>Questions & Answers</h5>
+                    <p>Ask questions on Questions&Answers section to access professional information and expertise or
+                        answer questions and
+                        share your valuable information with others</p>
                 </div>
             </div>
             <div class="col-md-4 intro-feature">
-                <div class="intro-icon">
-                    <span data-icon="&#xe046;" class="icon"></span>
-                </div>
                 <div class="intro-content last">
-                    <h5>Responsive Development</h5>
-                    <p>Built using the latest web technologies like html5, css3, and jQuery, rest assured Sedna will look smashing on every device under the sun.</p>
+                    <h5>Track Events</h5>
+                    <p>Track test engineering related events and improve your career in testing!</p>
                 </div>
             </div>
         </div>
     </div>
+    {{--<div class="down-arrow floating-arrow"><a href="#"><i class="fa fa-angle-down"></i></a></div>--}}
 </section>
-<section class="features section-padding" id="features">
+<section class="question section-padding" id="question">
     <div class="container">
         <div class="row">
             <div class="col-md-5 col-md-offset-7">
                 <div class="feature-list">
                     <h3>Sedna will drive your product forward</h3>
                     <p>Present your product, start up, or portfolio in a beautifully modern way. Turn your visitors in to clients.</p>
-                    <ul class="features-stack">
+                    <ul class="question-stack">
                         <li class="feature-item">
                             <div class="feature-icon">
                                 <span data-icon="&#xe03e;" class="icon"></span>
@@ -154,7 +162,7 @@ $url_css_js="http://localhost/laravel/resources/assets/";
     </div>
     <div class="responsive-feature-img"><img src="{{$url_css_js}}img/devices.png" alt="responsive devices"></div>
 </section>
-<section class="features-extra section-padding" id="assets">
+<section class="question-extra section-padding" id="assets">
     <div class="container">
         <div class="row">
             <div class="col-md-5">
@@ -208,7 +216,7 @@ $url_css_js="http://localhost/laravel/resources/assets/";
             <div class="col-md-4">
                 <article class="blog-post">
                     <figure>
-                        <a href="{{$url_css_js}}img/blog-img-01.jpg" class="single_image">
+                        <a href="img/blog-img-01.jpg" class="single_image">
                             <div class="blog-img-wrap">
                                 <div class="overlay">
                                     <i class="fa fa-search"></i>
@@ -226,7 +234,7 @@ $url_css_js="http://localhost/laravel/resources/assets/";
             <div class="col-md-4">
                 <article class="blog-post">
                     <figure>
-                        <a href="{{$url_css_js}}img/blog-img-02.jpg" class="single_image">
+                        <a href="img/blog-img-02.jpg" class="single_image">
                             <div class="blog-img-wrap">
                                 <div class="overlay">
                                     <i class="fa fa-search"></i>
@@ -244,7 +252,7 @@ $url_css_js="http://localhost/laravel/resources/assets/";
             <div class="col-md-4">
                 <article class="blog-post">
                     <figure>
-                        <a href="{{$url_css_js}}img/blog-img-03.jpg" class="single_image">
+                        <a href="img/blog-img-03.jpg" class="single_image">
                             <div class="blog-img-wrap">
                                 <div class="overlay">
                                     <i class="fa fa-search"></i>
@@ -290,21 +298,22 @@ $url_css_js="http://localhost/laravel/resources/assets/";
         </div>
     </div>
 </section>
-<section class="sign-up section-padding text-center" id="login">
+<section class="sign-up section-padding text-center" id="download">
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <h3>Get started with Sedna, absolutely free</h3>
                 <p>Grab your copy today, exclusively from Codrops</p>
-                <form class="signup-form" action="auth/login" method="POST" role="form">
-                    {{ csrf_field() }}
+                <form class="signup-form" action="#" method="POST" role="form">
                     <div class="form-input-group">
-                        <i class="fa fa-envelope"></i><input type="text" name="email" class="" placeholder="E-mail Adresinizi Girin" required>
+                        <i class="fa fa-envelope"></i><input type="text" class="" placeholder="Enter your email"
+                                                             required>
                     </div>
                     <div class="form-input-group">
-                        <i class="fa fa-lock"></i><input type="password" name="password" class="" placeholder="Şifrenizi Girin" required>
+                        <i class="fa fa-lock"></i><input type="text" class="" placeholder="Enter your password"
+                                                         required>
                     </div>
-                    <button type="submit" class="btn-fill sign-up-btn">GİRİŞ YAP</button>
+                    <button type="submit" class="btn-fill sign-up-btn">Sign up for free</button>
                 </form>
             </div>
         </div>
@@ -322,27 +331,32 @@ $url_css_js="http://localhost/laravel/resources/assets/";
 <footer>
     <div class="container">
         <div class="row">
-            <div class="social-share">
-                <p>BİZİ ARKADAŞLARINIZLA PAYLAŞIN</p>
-                <a href="https://twitter.com/peterfinlan" class="twitter-share"><i class="fa fa-twitter"></i></a>
-                <a href="#" class="facebook-share"><i class="fa fa-facebook"></i></a>
-            </div>
-
             <div class="col-md-7">
                 <div class="footer-links">
                     <ul class="footer-group">
-                        <li><a href="#">HAKKINDA</a></li>
-                        <li><a href="#">GİZLİLİK</a></li>
-                        <li><a href="#">İLETİŞİM</a></li>
+                        <li><a href="#">question</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Sign up</a></li>
+                        <li><a href="http://tympanus.net/codrops/licensing/">Licence</a></li>
+                        <li><a href="http://tympanus.net/codrops/">Codrops</a></li>
+                        <li><a href="http://www.peterfinlan.com/">Peter Finlan</a></li>
                     </ul>
-                    <p>Copyright © 2015 <a href="http://www.havelsan.com.tr" target="_blank">Havelsan A.Ş.</a><br></p>
+                    <p>Copyright © 2015 <a href="#">Sedna</a><br>
+                        <a href="http://tympanus.net/codrops/licensing/">Licence</a> | Crafted with <span
+                                class="fa fa-heart pulse2"></span> by <a href="http://www.peterfinlan.com/">Peter
+                            Finlan</a>.</p>
                 </div>
             </div>
-
+            <div class="social-share">
+                <p>Share Sedna with your friends</p>
+                <a href="https://twitter.com/peterfinlan" class="twitter-share"><i class="fa fa-twitter"></i></a> <a
+                        href="https://www.facebook.com/pages/Codrops/159107397912" class="facebook-share"><i
+                            class="fa fa-facebook"></i></a>
+            </div>
         </div>
     </div>
 </footer>
-<script src="{{$url_css_js}}js/jquery-2.1.3.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="{{$url_css_js}}js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 <script src="{{$url_css_js}}bower_components/retina.js/dist/retina.js"></script>
 <script src="{{$url_css_js}}js/jquery.fancybox.pack.js"></script>
@@ -351,33 +365,29 @@ $url_css_js="http://localhost/laravel/resources/assets/";
 <script src="{{$url_css_js}}js/jquery.flexslider-min.js"></script>
 <script src="{{$url_css_js}}bower_components/classie/classie.js"></script>
 <script src="{{$url_css_js}}bower_components/jquery-waypoints/lib/jquery.waypoints.min.js"></script>
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='//www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
+<!-- For the demo ad only -->
+<script src="http://tympanus.net/codrops/adpacks/demoad.js"></script>
 <script type="text/javascript">
-    $(document).ready(function(){
+    $(document).ready(function () {
         //buraya login olunan kullanıcının ismini alıp (başarılı bir login gerçekleşmişse):
         // 1) header'daki Kayıt Ol butonunu Çıkış ile değiştiren ve Giriş butonunu login olunan kullanıcı adı yapan,
         // 2) Kullanıcı adına tıklandığında profil düzenleme sayfasını açan,
         // 3) Giriş yapıldıktan sonra sayfanın en altında bulunan form kısmını hidden yapan,
         // 4) Çıkış butonuna basıldığında sayfanın ilk haline geri getiren script gelecek.
-       $user_auth = {{Auth::Check()}} + ""
+        $user_auth = {{Auth::Check()}} +""
 
-        if($user_auth){
-            $user_name="<?php if(Auth::check()){echo Auth::user()->name;} ?>";
-            $user_id = "<?php if(Auth::check()){echo Auth::user()->id;} ?>";
+        if ($user_auth) {
+            $user_name = "<?php if (Auth::check()) {
+                echo Auth::user()->name;
+            } ?>";
+            $user_id = "<?php if (Auth::check()) {
+                echo Auth::user()->id;
+            } ?>";
             $("li#users a").html($user_name);
-            $("li#users a").attr("href" , "/profile/" + $user_id);
+            $("li#users a").attr("href", "/profile/" + $user_id);
             $("li#logout a").html("Çıkış Yap");
-            $("li#logout a").attr("href" , "/auth/logout");
+            $("li#logout a").attr("href", "/auth/logout");
             $("#login").hide();
-
         }
     });
 </script>
